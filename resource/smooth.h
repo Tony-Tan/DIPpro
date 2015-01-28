dfcfbb68d501256c2f5b3997d275c7564acb8f4d
Tony_Tan <tony.tan1991@gmail.com>
2015-01-28 17:46:17 +0800

    smooth method add

--- resource/smooth.h
+++ resource/smooth.h
@@ -8,6 +8,11 @@
 #define SMOOTH_MEDIAN
 #define SMOOTH_BILATERAL
 #define SMOOTH_MEAN
+#define NLMF_FUN_A 0   //arithmetic
+#define NLMF_FUN_H 1
+#define NLMF_FUN_G 2
+#define NLMF_MASK_M 0
+#define NLMF_MASK_G 1
 static double Distance(int x,int y,int c_x,int c_y);
 void MedianFilter(IplImage *src,IplImage *dst,int width,int height);
 void GaussianMask(double *mask,int width,int height,double deta);
@@ -17,4 +22,6 @@ double BilateralWindow(double *window,int width,int height,double deta_d,double 
 void BilateralFilter(IplImage *src,IplImage *dst,int width,int height,double deta_d,double deta_r);
 void Smooth(IplImage *src,IplImage *dst,int Smooth_type,int width,int height);
 void MeanFilter(IplImage *src,IplImage *dst,int width,int height);
+void NoLinearMeanFilter(IplImage *src,IplImage *dst,int width,int height,int Mask_type,int function_type,double param);
+void RobustSmoothFilter(IplImage *src,IplImage *dst,int width,int height);
 #endif
